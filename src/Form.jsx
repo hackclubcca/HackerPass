ad8a06648fc36aa619c3a52c3584973346bc69b2
Harris Beg <harris@harrisbeg.com>
2019-08-26 18:49:43 -0700

    Easier file tree -> + accessible

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
-import { FORM_FIELDS } from "../config/fields";
+import { FORM_FIELDS } from "./config/fields";
 import axios from 'axios';
 
 require('dotenv').config();
